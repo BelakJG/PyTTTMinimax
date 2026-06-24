d5c227cd37de236ac2040717e7eefddc2facf8d1
BelakJG <74761299+BelakJG@users.noreply.github.com>
2026-06-24 17:47:16 -0400

    Added functions to find minimax score based on human player

--- main.py
+++ main.py
@@ -22,13 +22,61 @@ def check_winner(board_string):
         return 1 if board_string[0] == player else -1
     if board_string[6] != "." and (board_string[6] == board_string[4] == board_string[2]):
         return 1 if board_string[6] == player else -1
+    #check draw
+    if "." not in board_string:
+        return 0
     #no winner
-    return 0
+    return None
 
+@cache
+def minimax(current_turn, board_string):
+    global player
+    winner = check_winner(board_string)
+    if winner is not None:
+        return winner
 
+    possible_moves = []
+    for i in range(9):
+        if board_string[i] == ".":
+            new_string = board_string[:i] + current_turn + board_string[i + 1:]
+            possible_moves.append(new_string)
+    if not possible_moves:
+        return 0
+
+    move_scores = []
+    for string in possible_moves:
+        move_scores.append(minimax("O" if current_turn == "X" else "X", string))
+
+    if current_turn == player:
+        return max(move_scores)
+    else:
+        return min(move_scores)
+
+def find_best(board_string):
+    global player
+    moves = []
+    for i in range(9):
+        if board_string[i] == ".":
+            new_string = board_string[:i] + player + board_string[i + 1:]
+            moves.append(new_string)
+
+    scores = []
+    for move in moves:
+        scores.append(minimax("O" if player == "X" else "X", move))
+    return moves[scores.index(max(scores))]
 
 # Press the green button in the gutter to run the script.
 if __name__ == "__main__":
-    string = "XO..X.OOX"
-    print_board(string)
-    print(check_winner(string))
+    string = "........."
+    player = "X" if int(input("Are player 1 or 2?: ")) == 1 else "O"
+    if player == "O":
+        print_board(string)
+        index = int(input("What position did your opponent play?: "))
+        string = string[:index] + ("O" if player == "X" else "X") + string[index + 1:]
+    while string.count(".") > 0:
+        print("Your best move is:")
+        string = find_best(string)
+        print_board(string)
+        index = int(input("What position did your opponent play?: "))
+        string = string[:index] + ("O" if player == "X" else "X") + string[index + 1:]
+        print("\n" * 25)
